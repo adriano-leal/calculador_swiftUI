6825473f1174a768ce1a672758eab3ea9c8f0af3
adriano <adriano.ramos@realizecfi.com.br>
2020-04-05 01:16:59 -0300

    Initial Commit

---
+++ Calculator_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator_SwiftUI
+//
+//  Created by Adriano Ramos on 05/04/20.
+//  Copyright © 2020 Adriano Ramos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
