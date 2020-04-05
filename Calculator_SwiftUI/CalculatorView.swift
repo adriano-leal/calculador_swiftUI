0a2b66cc3c2c764d10ca08cf797b7e7e63fb09fc
adriano <adriano.ramos@realizecfi.com.br>
2020-04-05 02:19:48 -0300

    first commit

---
+++ Calculator_SwiftUI/CalculatorView.swift
@@ -0,0 +1,21 @@
+//
+//  CalculatorView.swift
+//  Calculator_SwiftUI
+//
+//  Created by Adriano Ramos on 05/04/20.
+//  Copyright © 2020 Adriano Ramos. All rights reserved.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView()
+    }
+}
